eaac77286aa5b63b7bd91da1f8c4c430ca46f550
Saks777 <sb.sakenyunusov@gmail.com>
2023-04-01 14:15:58 +0800

    1

--- test.py
+++ test.py
@@ -1,29 +1,32 @@
-a = 5
-b = 10
-x = 'Hello world'
-print(a)
-print(b)
-print(x)
-print(a, ',', b, ',', x)            # 1 способ
-print('{} ,{} , {}'.format(a, b, x))  # 2 способ
-print(f'{a} ,{b} , {x}')            # 3 способ
+# a = 5
+# b = 10
+# x = 'Hello world'
+# print(a)
+# print(b)
+# print(x)
+# print(a, ',', b, ',', x)            # 1 способ
+# print('{} ,{} , {}'.format(a, b, x))  # 2 способ
+# print(f'{a} ,{b} , {x}')            # 3 способ
 
-list = [a, b, x]                      # список
-print(list)
+# list = [a, b, x]                      # список
+# print(list)
 
-"""print('Put your number')
-a = int(input())
-print(a)"""          
+# """print('Put your number')
+# a = int(input())
+# print(a)"""          
                 
-a = 1.355
-b = 3
-c = round(a*b, 2)   # 2 указывает на количество цифр после запятой
-print(c)
+# a = 1.355
+# b = 3
+# c = round(a*b, 2)   # 2 указывает на количество цифр после запятой
+# print(c)
 
-g = [1, 2, 3]
-#print(g)
-#print(not 2 in g)
-is_odd = g[0] % 2 == 0
-print(is_odd)
+# g = [1, 2, 3]
+# #print(g)
+# #print(not 2 in g)
+# is_odd = g[0] % 2 == 0
+# print(is_odd)
 
+a = '45+4'
+result = eval(a)
+print(result)
 
